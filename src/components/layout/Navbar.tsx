f1f3988e7ff35a42c9393df446f330bb1222ba32
Расул Ерматов <rasulermatov015@gmail.com>
2026-01-27 19:46:20 +0600

    fixed style

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
+
 export const Navbar = () => {
     return (
-        <div className="max-w-7xl mx-auto">
-          <nav className="flex flex-row justify-between  mx-auto items-center bg-[#F9FAFB] rounded-2xl px-4 py-4 mt-4">
-                <Image src="/icon/logo.svg" alt='icon'  width={52} height={52}/>
-                <div className="hidden md:flex space-x-6 text-sm font-medium text-gray-600">
-                  <Link href="/">Полезная информация</Link>
-                  <Link href="/about">О нас</Link>
-                  <Link href="/contacts">Контакты</Link>
+        <div className="max-w-7xl ml-20 px-4"> 
+          <nav className="flex flex-row justify-between items-center bg-[#F9FAFB] rounded-2xl px-6 py-4 mt-4 shadow-sm">
+                <Image src="/icon/logo.svg" alt='icon' width={52} height={52}/>
+                
+                <div className="hidden md:flex space-x-8 text-sm font-bold text-gray-600">
+                  <Link href="/" className="hover:text-black transition-colors">Полезная информация</Link>
+                  <Link href="/about" className="hover:text-black transition-colors">О нас</Link>
+                  <Link href="/contacts" className="hover:text-black transition-colors">Контакты</Link>
                 </div>
-                <button className="bg-red-500 text-white px-6 py-2 rounded-lg text-xs font-bold">Регистрация</button>
-        </nav>
+                
+                <button className="bg-[#E11D48] text-white px-6 py-2.5 rounded-xl text-xs font-black hover:bg-[#BE123C] transition-all">
+                    Регистрация
+                </button>
+          </nav>
         </div>
     )
 }
